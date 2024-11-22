0a743dabb81449abbaaaa257a64cfaca5e3720ed
Heyuuuuu <44488707+Heyuuuuu@users.noreply.github.com>
2024-11-22 18:07:55 +0800

    升级到2019版引擎

--- Assets/Intro_Heli_Physics/Code/Scripts/Characteristics/Heli_Characteristics.cs
+++ Assets/Intro_Heli_Physics/Code/Scripts/Characteristics/Heli_Characteristics.cs
@@ -9,9 +9,25 @@ namespace Intro_Heli_Physics
         [Header("Lift Properties")]
         public float maxLiftForce = 100f;
 
-        public float maxRotorTourque = 10f;
-
         public HeliMain_Rotors mainRotors;
+
+        [Space]
+        [Header("Tail Rotor Properties")]
+        public float tailForce = 2f;
+
+        [Space]
+        [Header("Cyclic Properties")]
+        public float cyclicForce = 2f;
+
+        [Header("Auto Level Properties")]
+        public float autoLevelForce = 2f;
+
+        private Vector3 flatFwd;
+        private Vector3 flatRight;
+
+        private float forwardDot;
+        private float rightDot;
+
         #endregion
 
         #region Builtin Methods
@@ -33,8 +49,12 @@ namespace Intro_Heli_Physics
         public void UpdateCharacteristics(Rigidbody rb, Input_Controller input)
         {
             HandleLift(rb, input);
-            HandleCyclic();
+            HandleCyclic(rb, input);
             HandlePedals(rb, input);
+
+            CalculateAngles();
+
+            AutoLevel(rb);
         }
 
         protected virtual void HandleLift(Rigidbody rb, Input_Controller input)
@@ -50,14 +70,48 @@ namespace Intro_Heli_Physics
             }
         }
 
-        protected virtual void HandleCyclic()
+        protected virtual void HandleCyclic(Rigidbody rb, Input_Controller input)
         {
 
+            float finalCylicForceX = input.Cyclic.x * cyclicForce;
+            rb.AddRelativeTorque(Vector3.forward * finalCylicForceX, ForceMode.Acceleration);
+
+            float finalCylicForceY = input.Cyclic.y * cyclicForce;
+            rb.AddRelativeTorque(Vector3.right * finalCylicForceY, ForceMode.Acceleration);
         }
 
         protected virtual void HandlePedals(Rigidbody rb, Input_Controller input)
         {
-            rb.AddTorque(transform.up * input.Pedal * maxRotorTourque);
+            rb.AddTorque(transform.up * input.Pedal * tailForce, ForceMode.Acceleration);
+        }
+
+        protected void CalculateAngles()
+        {
+            flatFwd = transform.forward;
+            flatFwd.y = 0;
+            flatFwd.Normalize();
+
+            Debug.DrawRay(transform.position, flatFwd, Color.blue);
+
+            flatRight = transform.right;
+            flatRight.y = 0;
+            flatRight.Normalize();
+
+            Debug.DrawRay(transform.position, flatRight, Color.red);
+
+            forwardDot = Vector3.Dot(transform.up, flatFwd);
+            rightDot = Vector3.Dot(transform.up, flatRight);
+
+           // Debug.Log($"===rightDot:{forwardDot} , rightDot:{rightDot}");
+        }
+
+        protected void AutoLevel(Rigidbody rb)
+        {
+            float rightForce = - forwardDot * autoLevelForce;
+            float forwardForce = rightDot * autoLevelForce;
+
+            rb.AddRelativeTorque(Vector3.forward * forwardForce, ForceMode.Acceleration);
+            rb.AddRelativeTorque(Vector3.right * rightForce, ForceMode.Acceleration);
         }
 
         #endregion
